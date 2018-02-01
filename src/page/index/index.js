74f8aee71f7c109aabe9219cf8e957cea3fecca5
Sivey Tan <562768172@qq.com>
2018-02-01 13:11:21 +0800

    initial

---
+++ src/page/index/index.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: Sam958
+ * @Date:   2018-1-15
+ * @Last modified by:   Sam958
+ * @Last modified time: 2018-1-15
+ */
+'use strict';
+
+require('./index.css');
+require('../module.js');
